533ad5fa34655570ade0d027e6f728b3ff49eda5
joe73K <j@teamlucid.com>
2012-05-16 05:30:17 +1000

    Removed the FormBuilder assignment to ensure Twitter-Bootstrap would work.

--- lib/simple_captcha/engine.rb
+++ lib/simple_captcha/engine.rb
@@ -14,13 +14,14 @@ module SimpleCaptcha
       ActionView::Base.send(:include, SimpleCaptcha::ViewHelper)
       ActionView::Helpers::FormBuilder.send(:include, SimpleCaptcha::FormBuilder)
 
-      if Object.const_defined?("Formtastic")
-        if Formtastic.const_defined?("Helpers")
-          Formtastic::Helpers::FormHelper.builder = SimpleCaptcha::CustomFormBuilder
-        else
-          Formtastic::SemanticFormHelper.builder = SimpleCaptcha::CustomFormBuilder
-        end
-      end
+# removed by Joe
+#      if Object.const_defined?("Formtastic")
+#        if Formtastic.const_defined?("Helpers")
+#          Formtastic::Helpers::FormHelper.builder = SimpleCaptcha::CustomFormBuilder
+#        else
+#          Formtastic::SemanticFormHelper.builder = SimpleCaptcha::CustomFormBuilder
+#        end
+#      end
     end
 
     config.app_middleware.use SimpleCaptcha::Middleware
